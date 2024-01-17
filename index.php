dd94122854cc706900b37864a557e0cec3ce8490
Antrussian <antonino.russo.agent@gmail.com>
2024-01-17 01:18:12 +0100

    creo frontend di base con bootstrap e creo struttura vue in main.js

--- index.php
+++ index.php
@@ -1,24 +1,43 @@
 <!DOCTYPE html>
-<html lang="en">
+<html lang="it">
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
-    <link href="https://cdnjs.cloudflare.com/ajax/libs/axios/1.6.5/axios.min.js">
+    <title>MyPlaylist</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">   
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/axios/1.6.5/axios.min.js"></script>
+    <script src='https://unpkg.com/vue@3/dist/vue.global.js'></script>
 
 </head>
 <body>
     
 
 
+<nav class="navbar navbar-dark bg-dark p-3">
+  <a class="navbar-brand" href="#">
+    <img src="https://cdn-icons-png.flaticon.com/256/2111/2111624.png" width="30" height="30" alt="logo">
+  </a>
+</nav>
+
+
+
+<main class="bg-dark p-5">
+
+<div class="card" style="width: 18rem;">
+  <img class="card-img-top" src="https://upload.wikimedia.org/wikipedia/pt/thumb/3/32/AmericanIdiot.jpg/220px-AmericanIdiot.jpg" alt="Card image cap">
+  <div class="card-body">
+    <p class="card-text">Album</p>
+    <p class="card-text">Autore</p>
+    <p class="card-text">Anno</p>
+  </div>
+</div>
+</main>
 
 
 
 
 
-
-<script src='https://unpkg.com/vue@3/dist/vue.global.js'></script>
+<script src="./js/main.js"></script>
 </body>
 </html>
