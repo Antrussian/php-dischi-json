294e44f5b27e8a295db55a38aa8a3d1a03545c83
Antrussian <antonino.russo.agent@gmail.com>
2024-01-17 02:42:38 +0100

    Fine Esercizio.

--- index.php
+++ index.php
@@ -8,42 +8,33 @@
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">   
     <script src="https://cdnjs.cloudflare.com/ajax/libs/axios/1.6.5/axios.min.js"></script>
     <script src='https://unpkg.com/vue@3/dist/vue.global.js'></script>
-
 </head>
 <body>
-<div id="app">
-
+    <div id="app">
         <nav class="navbar navbar-dark bg-dark p-3">
             <a class="navbar-brand" href="#">
                 <img src="https://cdn-icons-png.flaticon.com/256/2111/2111624.png" width="30" height="30" alt="logo">
             </a>
         </nav>
 
-
-
-        <main class="bg-dark">
-
-            <div class="card" style="width: 18rem;">
-     
-                <div class="card-body "  v-for="(element, index) in albums">
-                <img class="card-img-top" :src="element.copertina" alt="Card image cap" @click="openModal(album)">
-                         <h2 class="card-text"> {{ element.titolo }}</h2>
-                         <h3 class="card-text"> {{ element.autore }}</h3>
-                         <p class="card-text"> {{ element.anno }}</p>
-
-                
-
-                 
-               
-
+        <div class="container-fluid bg-dark">
+            <div class="row justify-content-center">
+                <div class="col-8 p-3">
+                    <div class="d-flex flex-wrap justify-content-center">
+                        <div class="card m-3" style="width: 18rem;" v-for="(element, index) in albums" :key="index">
+                            <div class="card-body text-center">
+                                <img class="card-img-top" :src="element.copertina" alt="Card image cap">
+                                <h4 class="card-text p-1 mt-3"> {{ element.titolo }}</h2>
+                                <h5 class="card-text p-1"> {{ element.autore }}</h3>
+                                <p class="card-text p-1"> {{ element.anno }}</p>
+                            </div>
+                        </div>
+                    </div>
                 </div>
             </div>
-        </main>
+        </div>
 
-
-</div>
-
-
-<script src="./js/main.js"></script>
+        <script src="./js/main.js"></script>
+    </div>
 </body>
 </html>
